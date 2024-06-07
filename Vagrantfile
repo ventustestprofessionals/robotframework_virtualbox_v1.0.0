2c172bb339f347948c942cd593010414cc0e92d4
harmbruinsapi <harmbruins@gmail.com>
2024-06-07 12:24:06 +0200

    fixed robotframework

--- Vagrantfile
+++ Vagrantfile
@@ -4,7 +4,6 @@
 Vagrant.configure("2") do |config|
   #desktop
   config.vm.box = "skaary/LinuxMint21"
-  config.vm.box_version = "1"
 
   config.vm.network "private_network", ip: "192.168.33.99"
 
--- provision.sh
+++ provision.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
-
 echo "########################"
-echo "# Install net-tools"
+echo "# Install net.tools     "
 echo "########################"
 sudo apt-get -y install net-tools
-
 echo "########################"
-echo "# Install python-pip"
+echo "# PIP and python        "
 echo "########################"
-sudo apt -y install python3-pip
-
+sudo apt-get -y install net-tools
+sudo apt-get -y install python
 echo "########################"
-echo "# PIP Install robotframework"
+echo "# Update machine        "
+echo "########################"
+apt-get update
+sudo apt install -y python3-pip
+echo "########################"
+echo "# install robotframework"
 echo "########################"
 pip install robotframework
+robot --version
+echo "############################"
+echo "# install visual studio code"
+echo "############################"
+sudo mv /etc/apt/preferences.d/nosnap.pref ~
+sudo apt install snapd
+sudo snap install --classic code
+
+
